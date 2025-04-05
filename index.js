db06e5604aae2b2b1f3fac3ba0c634149e269645
gdtech1026 <udacitywww1216@gmail.com>
2025-04-05 10:47:16 -0400

    update index2

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ io.on("connection", (socket) => {
 app.get('/', (req, res) => res.send('Hello World!'));
 
 server.listen(3000,  () => {
-  console.log('Server listening on http://0.0.0.0:8000');
+  console.log('Server listening on http://0.0.0.0:3000');
 });
